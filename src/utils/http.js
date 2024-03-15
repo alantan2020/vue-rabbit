4c3a0f0942b1efbf07a1c7b1ffecc5c608a159f8
alantan2020 <582704597@qq.com>
2024-03-15 23:48:16 +0800

    layout-静态

--- src/utils/http.js
+++ src/utils/http.js
@@ -11,13 +11,6 @@ const httpInstance = axios.create({
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
-    // // 1. 从pinia获取token数据
-    // const userStore = useUserStore()
-    // // 2. 按照后端的要求拼接token数据
-    // const token = userStore.userInfo.token
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`
-    // }
     return config
   },
   (e) => Promise.reject(e)
